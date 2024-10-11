40dae10a7807882b42a0a2cc0450156ff90678fc
Mikwuegbu <mstephen130@yahoo.com>
2024-10-11 16:24:04 +0100

    mongoDb/model

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const express = require('express'); // use express library
+const mongoose = require('mongoose'); // connect to MongoDb
 
+const app = express();
+
+// sample get request
+app.get('/', (req, res) => {
+	res.send(`Hello, World! this is Mark`);
+});
+
+// Connect to MongoDB & listen on port 3000
+mongoose
+	.connect(
+		'mongodb+srv://admin:admin@backenddb.s4y5p.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=BackendDB'
+	)
+	.then(() => {
+		console.log('Connected to the Database');
+		app.listen(3000, () => {
+			console.log('Server is running on port 3000');
+		});
+	})
+	.catch(() => {
+		console.log('Could not connect to the database');
+	});
